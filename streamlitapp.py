346ff891ecaf9125b1e85c7c3af23b1d08230954
Jia Ming Low <66296475+Jming9638@users.noreply.github.com>
2022-05-22 12:19:27 +0800

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -62,7 +62,8 @@ def run():
         with col2[0]:
             ph11 = st.empty()
             rn = ph11.text_input('Which row?')
-            rn = int(rn)
+            if rn is not None and rn != '':
+                rn = int(rn)
             
         with col2[1]:
             ph22 = st.empty()
