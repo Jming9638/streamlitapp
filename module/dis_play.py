fb988a073c73d76bce167e37398d9817a62dba34
Jia Ming Low <66296475+Jming9638@users.noreply.github.com>
2022-05-22 18:53:13 +0800

    Add files via upload

---
+++ module/dis_play.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+def summary_res(result):
+    st.header("Summary:")
+    col = st.columns([1,2,1,1,1])
+    for i in result.columns:
+        for j in result.index:
+            amount = result.loc[j,i]
+            if amount > 0:
+                with col[0]:
+                    st.write(i)
+                with col[1]:
+                    st.write("have to pay")
+                with col[2]:
+                    st.write("RM {:.2f}".format(amount))
+                with col[3]:
+                    st.write("to")
+                with col[4]:
+                    st.write(j)
